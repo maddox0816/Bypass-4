6a8224941ca1c2b8e29b9392edff45665ab67f79
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2021-10-04 21:00:28 -0700

    commit 3

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,3 @@
-/*
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
@@ -17,21 +16,4 @@ proxy.bundleScripts();
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
     response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(443);
-*/
-
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const server = http.createServer();
-const Corrosion = require('../');
-const proxy = new Corrosion({
-    codec: 'xor',
-});
-
-proxy.bundleScripts();
-
-server.on('request', (request, response) => {
-    if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
-    response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(80);
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(7070); // we have to listen at a higher port because heroku
