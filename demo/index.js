fac7ba513bb5836e76d4b418894aebfd3898b08c
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2021-10-04 21:04:27 -0700

    commit 4 or something

--- demo/index.js
+++ demo/index.js
@@ -16,4 +16,4 @@ proxy.bundleScripts();
 server.on('request', (request, response) => {
     if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
     response.end(fs.readFileSync(__dirname + '/index.html', 'utf-8'));
-}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(7070); // we have to listen at a higher port because heroku
+}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(process.env.PORT || 7070); // we have to listen at a higher port because heroku
